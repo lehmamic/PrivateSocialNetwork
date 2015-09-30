714abf342cc726bd992dec867e912300ed084bf3
Michael Lehmann <leh@zuehlke.com>
2015-09-30 17:29:31 +0200

    chore(gulp): Setup initial gulp build.

--- src/PrivateSocialNetwork.Portal.Web/client/components/app/app.ts
+++ src/PrivateSocialNetwork.Portal.Web/client/components/app/app.ts
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="../../../typings/angular2/angular2.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+    selector: 'socal-network-client'
+})
+@View({
+    template: '<h1>Hello {{ name }}</h1>'
+})
+export class PrivateSocialNetworkClient {
+    name:string = 'Michael';
+}
---
+++ src/PrivateSocialNetwork.Portal.Web/client/main.ts
@@ -0,0 +1,6 @@
+﻿/// <reference path="../typings/angular2/angular2.d.ts" />
+
+import {bootstrap} from 'angular2/angular2';
+import {PrivateSocialNetworkClient} from './components/app/app';
+
+bootstrap(PrivateSocialNetworkClient);
