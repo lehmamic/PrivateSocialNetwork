cb9bd968ccd6fa03325367b780e8ca85912e6781
Michael Lehmann <lehmamic@bluewin.ch>
2015-10-16 09:45:05 +0200

    feat(router): bootstrap the router bindings

--- src/PrivateSocialNetwork.Portal.Web.Client/app/main.ts
+++ src/PrivateSocialNetwork.Portal.Web.Client/app/main.ts
@@ -1,4 +1,8 @@
-﻿import {bootstrap} from 'angular2/angular2';
+﻿import {bootstrap, bind} from 'angular2/angular2';
+import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
 import {PrivateSocialNetworkClient} from './components/social-network-client/social-network-client';
 
-bootstrap(PrivateSocialNetworkClient);
+bootstrap(PrivateSocialNetworkClient, [
+	ROUTER_PROVIDERS,
+	bind(LocationStrategy).toClass(HashLocationStrategy)
+]);
